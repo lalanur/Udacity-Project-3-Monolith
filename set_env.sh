2804b102dc9102b02501cdbc57fd8777e5d349db
unknown <lalanura5@gmail.com>
2022-11-14 14:12:09 +0000

    Adding CI

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=laladb
+export POSTGRES_PASSWORD=Festac1234
+export POSTGRES_HOST=database-1.cpf9biphtalr.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=zahrabucket
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
