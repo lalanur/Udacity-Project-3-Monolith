aa61b3ac83c8f6d7ef4eb1a4e13e23cc4e3fce28
unknown <lalanura5@gmail.com>
2022-11-17 21:33:19 +0000

    my kubectl

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,10 @@ kubectl delete deploy reverseproxy
 kubectl delete deploy frontend
 
 kubectl apply -f feed-deploy.yaml
+kubectl apply -f feed-service.yaml
 kubectl apply -f user-deploy.yaml
+kubectl apply -f user-service.yaml
 kubectl apply -f reverseproxy-deploy.yaml
+kubectl apply -f reverseproxy-service.yaml
 kubectl apply -f frontend-deploy.yaml
+kubectl apply -f frontend-serivce.yaml
